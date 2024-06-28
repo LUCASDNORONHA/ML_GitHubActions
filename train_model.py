96f2f4c52e0f92f7ed2ad8a38364da09416ae67b
Lucas Dias Noronha <lucasdiasnoronha1@gmail.com>
2024-06-27 21:26:13 -0300

    Teste enviando código quebrado e observar o que acontece no workflow

--- train_model.py
+++ train_model.py
@@ -6,6 +6,7 @@ import pickle
 def load_dataset():
     """
     Carrega o conjunto de dados Wine do Scikit-Learn.
+
     Retorna:
     X (np.array): Dados de entrada.
     y (np.array): Rótulos de destino.
@@ -16,23 +17,33 @@ def load_dataset():
 def train_model(X : np.array, y : np.array) -> RandomForestClassifier:
     """
     Treina um modelo de floresta aleatória no conjunto de dados Wine.
+
     Parâmetros:
     X (np.array): Dados de entrada.
     y (np.array): Rótulos de destino.
+
     Retorna:
     model (RandomForestClassifier): O modelo treinado.
     """
+
     # Definir o modelo
-    model = RandomForestClassifier(n_estimators = 2, max_depth = 1, random_state = 1)
+    model = RandomForestClassifier(n_estimators = 15, 
+                                   max_depth = 3, 
+                                   n_neighbors = 120, 
+                                   random_state = 1)
+
     # Treinar o modelo
     model = model.fit(X, y)
+
     # Avaliar o treinamento do modelo
     print(f"Training Mean Accuracy: {model.score(X, y)}")
+
     return model
 
 def save_model(model, filepath):
     """
     Salva o modelo treinado em um arquivo pickle.
+
     Parâmetros:
     model (qualquer): O modelo treinado para ser salvo.
     filepath (str): O caminho do arquivo onde o modelo treinado será salvo.
@@ -43,15 +54,17 @@ def save_model(model, filepath):
 def main():
     """
     A função principal que executa as etapas de treinamento do modelo.
+
     Esta função carrega o conjunto de dados Wine usando a função load_dataset(), 
     treina um modelo de floresta aleatória nos dados carregados usando a função train_model(),
     e retorna o modelo treinado.
     """
     X, y = load_dataset()
     model = train_model(X, y)
-    save_model(model, 'model.pkl')
+    save_model(model, 'my_beautiful_model.pkl')
     
     return 
+
 
 if __name__ == "__main__":
     main()
